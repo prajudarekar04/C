2fa70a26b945689f9ae28957ed0d96d008bd3595
Prajakta Darekar <114302180+prajudarekar04@users.noreply.github.com>
2023-09-02 10:36:09 +0530

    Table1.java

---
+++ Java/Table1.java
@@ -0,0 +1,14 @@
+class Table1
+{
+	public static void main(String[]avgs)
+	{
+		int i=1;
+		int no=(int)(Math.random()*100);
+		System.out.println("Table");
+		while(i<=10)
+		{
+			System.out.print(" "+(no*i));
+			i++;
+		}
+	}
+}
