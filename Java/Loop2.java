2994175007bb961836e0e3729524391993ea4923
Prajakta Darekar <114302180+prajudarekar04@users.noreply.github.com>
2023-09-02 12:28:18 +0530

    Add files via upload

---
+++ Java/Loop2.java
@@ -0,0 +1,15 @@
+class Loop2
+{
+	public static void main(String []avgs)
+	{
+		int i=1;
+		while(i<=5)
+		{
+			System.out.println("Welcome "+i);	//concadination
+			i++;
+		}
+	}
+}
+/*
+Object class belong to java.lang,which is
+*/
